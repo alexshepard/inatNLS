097fe946295c0b418b09389456250266c92d7288
Alex Shepard <alex@meatfreezer.com>
2024-05-07 14:03:00 -0700

    oops, forgot to add this file

---
+++ requestFormatter.py
@@ -0,0 +1,14 @@
+import logging
+
+from flask import has_request_context, request
+
+
+class RequestFormatter(logging.Formatter):
+    def format(self, record):
+        if has_request_context():
+            record.url = request.url
+            record.remote_addr = request.remote_addr
+        else:
+            record.url = None
+            record.remote_addr = None
+        return super().format(record)
