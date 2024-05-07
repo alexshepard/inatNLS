35c9f678a8a87cf54fdf739d4b372e4f7f5fc8d9
Alex Shepard <alex@meatfreezer.com>
2024-05-07 14:02:47 -0700

    move some data for the UI out of app.py

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ import click
 from flask import Flask, render_template, request
 
 from config import Config
-from search import Search
+from data import iconic_taxa, continent_choices
 from requestFormatter import RequestFormatter
+from search import Search
+
 
 logging.basicConfig(
     level=logging.WARNING, format="%(asctime)s - %(levelname)s - %(message)s"
@@ -35,34 +37,6 @@ def create_app():
 
 
 app = create_app()
-
-
-iconic_taxa = {
-    "48460": "Life",
-    "48460/1/2/355675/20978": "Amphibians",
-    "48460/1": "Animals",
-    "48460/1/47120/245097/47119": "Arachnids",
-    "48460/1/2/355675/3": "Birds",
-    "48460/48222": "Chromistans",
-    "48460/47170": "Fungi",
-    "48460/1/47120/372739/47158": "Insects",
-    "48460/1/2/355675/40151": "Mammals",
-    "48460/1/47115": "Molluscs",
-    "48460/47126": "Plants",
-    "48460/47686": "Protozoans",
-    "48460/1/2/355675/47178": "Ray-finned Fishes",
-    "48460/1/2/355675/26036": "Reptiles",
-}
-
-continent_choices = [
-    "Worldwide",
-    "Africa",
-    "Asia",
-    "Europe",
-    "North America",
-    "Oceania",
-    "South America",
-]
 
 
 @app.get("/")
---
+++ data.py
@@ -0,0 +1,26 @@
+iconic_taxa = {
+    "48460": "Life",
+    "48460/1/2/355675/20978": "Amphibians",
+    "48460/1": "Animals",
+    "48460/1/47120/245097/47119": "Arachnids",
+    "48460/1/2/355675/3": "Birds",
+    "48460/48222": "Chromistans",
+    "48460/47170": "Fungi",
+    "48460/1/47120/372739/47158": "Insects",
+    "48460/1/2/355675/40151": "Mammals",
+    "48460/1/47115": "Molluscs",
+    "48460/47126": "Plants",
+    "48460/47686": "Protozoans",
+    "48460/1/2/355675/47178": "Ray-finned Fishes",
+    "48460/1/2/355675/26036": "Reptiles",
+}
+
+continent_choices = [
+    "Worldwide",
+    "Africa",
+    "Asia",
+    "Europe",
+    "North America",
+    "Oceania",
+    "South America",
+]
